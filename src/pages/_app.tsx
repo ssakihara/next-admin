9764bbcee6fe56d4126cbc5e1530f2cf9ccde110
SakiharaShinnosuke <sakihara.shinnosuke@gmail.com>
2021-04-12 22:08:37 +0900

    [update]add chakra-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import React from 'react';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default App;
